6b7c5cd7d13f53c02dbd35bd1019dd28772b9775
vegodiego <53877309+vegodiego@users.noreply.github.com>
2020-04-11 20:51:44 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className>
       <h1>Mi primera aplicación</h1>
-      <p>Esta es mi primera aplicación en React, está padrísimo</p>
+      <p>Esta es mi primera aplicación en React, está padrísimo!</p>
     </div>
   );
 }
